71285751bd5e080ebb4d978d33839d25f1afd819
David <itsmesquee@gmail.com>
2024-07-07 13:50:02 -0600

    added html to Js

--- app.js
+++ app.js
@@ -11,4 +11,18 @@ fetch('https://potterapi-fedeperin.vercel.app/en/spells')
         console.log(res)
     })
 
-		
+async function main() {
+    const spells = await fetch("https://potterapi-fedeperin.vercel.app/en/spells");
+    const spellsData = await spells.json();
+    console.log(
+    spellsData.map(spell => `<div class="spells">
+        <div class="spell--details">
+          <h1>Spell Name</h1>
+          <p class="spell__description">Description of spell</p>
+        </div>
+      </div>` )
+    .join("")
+);
+}
+
+main()
