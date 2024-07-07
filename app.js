c97575167b875c3d9aa1452ea6a12e30c01b0bcd
David <itsmesquee@gmail.com>
2024-07-06 22:08:12 -0600

    changed project to harry potter things

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const url = 'https://flight-fare-search.p.rapidapi.com/v2/flights/?from=LHR&to=DXB&adult=1&type=economy&currency=USD';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'Sign Up for Key',
-		'x-rapidapi-host': 'flight-fare-search.p.rapidapi.com'
-	}
-};
+const fetchSpells = async () => {
+	const res = await fetch('https://potterapi-fedeperin.vercel.app/en/spells')
+	const spells = await res.json()
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+	return spells
 }
+
+fetch('https://potterapi-fedeperin.vercel.app/en/spells')
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+    })
+
+		
