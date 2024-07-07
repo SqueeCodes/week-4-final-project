c023f1dd07f823fb3090c24bf34800fd7f931ec7
David <itsmesquee@gmail.com>
2024-07-07 16:43:50 -0600

    changed project to characters

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-const fetchSpells = async () => {
-	const res = await fetch('https://potterapi-fedeperin.vercel.app/en/spells')
-	const spells = await res.json()
+const fetchCharacters = async () => {
+	const res = await fetch('https://potterapi-fedeperin.vercel.app/en/characters')
+	const characters = await res.json()
 
-	return spells
+	return characters
 }
 
-fetch('https://potterapi-fedeperin.vercel.app/en/spells')
+fetch('https://potterapi-fedeperin.vercel.app/en/characters')
     .then(res => res.json())
     .then(res => {
         console.log(res)
     })
 
-const spellBoardEl = document.querySelector(".spell--board");
+const characterBoardEl = document.querySelector(".characters--board");
 
 
 
 async function main() {
-    const spells = await fetch("https://potterapi-fedeperin.vercel.app/en/spells");
-    const spellsData = await spells.json();
-    spellBoardEl.innerHTML = spellsData.map((spell) => spellHTML(spell)).join("");
+    const characters = await fetch("https://potterapi-fedeperin.vercel.app/en/characters");
+    const charactersData = await characters.json();
+    characterBoardEl.innerHTML = charactersData.map((character) => characterHTML(character)).join("");
 }
 
 main();
 
-function spellHTML(spell) {
-    return `<div class="spells">
-                <div class="spell--details">
-                    <h1>${spell.spell}</h1>
-                    <p class="spell__description">${spell.use}</p>
+function characterHTML(character) {
+    return `<div class="characters">
+                <div class="character--img__wrapper">
+                    <figure>
+                        <img class="character--img src="${character.image}" alt="">
+                    </figure>
                 </div>
+                <div class="character--details">
+                <h1 class="nickname">${character.nickname}</h1>
+                </div>
+            </div>`
+}
+
+function characterInfoHTML(character) {
+    return `<div class="character--img__wrapper">
+                <figure>
+                    <img class="character--img src="${character.image}" alt="">
+                </figure>
+            </div>
+            <div class="character--details">
+                <h1 class="fullname">${character.fullName}</h1>
+                <h2 class="hogwarts--house">${character.hogwartsHouse}</h2>
+                <h3 class="character--birthdate">${character.birthdate}</h3>                        
+                <h4 class="character--childs">${character.children}</h4>
+                <p class="played-by">${character.interpretedBy}</p>
             </div>`
 }
