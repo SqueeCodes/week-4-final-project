5eb3878bcbbe36d6603618aedf0bfa77dd1305d1
David <itsmesquee@gmail.com>
2024-07-09 11:15:44 -0600

    linked secondary js file

--- characters.js
+++ characters.js
@@ -1,1 +1,11 @@
-localStorage.get("id84");
+
+
+async function main() {
+  const id = localStorage.getItem("id84")
+  const posts = await fetch(`https://potterapi-fedeperin.vercel.app/en/characters?search=${}`);
+  const postsData = await posts.json();
+
+  console.log(postsData)
+}
+
+main();
