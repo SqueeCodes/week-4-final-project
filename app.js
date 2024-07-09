926f61e5cbd1fb1d0ecb1c5c7c3141a9066138cb
David <itsmesquee@gmail.com>
2024-07-09 14:07:54 -0600

    created functions to diplay none on characters with no kids

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const fetchCharacters = async () => {
     const res = await fetch('https://potterapi-fedeperin.vercel.app/en/characters');
     const characters = await res.json();
     return characters;
-  };
-  
-  const characterBoardEl = document.querySelector(".characters--board");
-  const searchInputEl = document.querySelector(".nav-bar__search--bar");
-  
-  async function main() {
-    const characters = await fetchCharacters();
-    displayCharacters(characters);
-  }
-  
-  function displayCharacters(characters) {
-    characterBoardEl.innerHTML = characters.slice(0, 8).map((character) => characterHTML(character)).join("");
-  }
-  
-  function characterHTML(character) {
+};
+
+const characterBoardEl = document.querySelector(".characters--board");
+const searchInputEl = document.querySelector(".nav-bar__search--bar");
+
+function getChildrenText(children) {
+    return children && children.length > 0 ? children.join(', ') : 'None';
+}
+
+function characterHTML(character) {
     return `<div class="characters">
               <div class="character--img__wrapper">
                 <figure>
@@ -27,14 +22,23 @@ const fetchCharacters = async () => {
                 <h1 class="fullname">${character.fullName}</h1>
                 <h2 class="hogwarts--house">${character.hogwartsHouse}</h2>
                 <h4 class="character--childs">Children: 
-                <br> ${character.children ? character.children.join(', ') : 'None'}</h4>
+                <br> ${getChildrenText(character.children)}</h4>
                 <p class="played-by">Played by: <br>${character.interpretedBy}</p>
                 <h3 class="character--birthdate">Born: ${character.birthdate}</h3>                        
               </div>
             </div>`;
-  }
-  
-  async function handleSearch() {
+}
+
+function displayCharacters(characters) {
+    characterBoardEl.innerHTML = characters.slice(0, 8).map((character) => characterHTML(character)).join("");
+}
+
+async function main() {
+    const characters = await fetchCharacters();
+    displayCharacters(characters);
+}
+
+async function handleSearch() {
     const query = searchInputEl.value.toLowerCase();
     const characters = await fetchCharacters();
     const filteredCharacters = characters.filter(character => 
@@ -43,9 +47,8 @@ const fetchCharacters = async () => {
       character.interpretedBy.toLowerCase().includes(query)
     );
     displayCharacters(filteredCharacters);
-  }
-  
-  searchInputEl.addEventListener("input", handleSearch);
-  
-  main();
-  
+}
+
+searchInputEl.addEventListener("input", handleSearch);
+
+main();
