dddb68984d56ce1c685fa8af059f6fc15d4efe8f
David <itsmesquee@gmail.com>
2024-07-09 14:01:57 -0600

    added functionality to search bar

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
 const fetchCharacters = async () => {
-	const res = await fetch('https://potterapi-fedeperin.vercel.app/en/characters')
-	const characters = await res.json()
-
-	return characters
-}
-
-fetch('https://potterapi-fedeperin.vercel.app/en/characters')
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
-    })
-
-const characterBoardEl = document.querySelector(".characters--board");
-
-
-
-async function main() {
-    const characters = await fetch("https://potterapi-fedeperin.vercel.app/en/characters");
-    const charactersData = await characters.json();
-    characterBoardEl.innerHTML = charactersData.map((character) => characterHTML(character)).join("");
-}
-
-main();
-
-function characterHTML(character) {
+    const res = await fetch('https://potterapi-fedeperin.vercel.app/en/characters');
+    const characters = await res.json();
+    return characters;
+  };
+  
+  const characterBoardEl = document.querySelector(".characters--board");
+  const searchInputEl = document.querySelector(".nav-bar__search--bar");
+  
+  async function main() {
+    const characters = await fetchCharacters();
+    displayCharacters(characters);
+  }
+  
+  function displayCharacters(characters) {
+    characterBoardEl.innerHTML = characters.slice(0, 8).map((character) => characterHTML(character)).join("");
+  }
+  
+  function characterHTML(character) {
     return `<div class="characters">
-                <div class="character--img__wrapper">
-                    <figure>
-                        <img class="character--img" src="${character.image}" alt="">
-                    </figure>
-                </div>
-                <div class="character--details">
+              <div class="character--img__wrapper">
+                <figure>
+                  <img class="character--img" src="${character.image}" alt="">
+                </figure>
+              </div>
+              <div class="character--details">
                 <h1 class="fullname">${character.fullName}</h1>
                 <h2 class="hogwarts--house">${character.hogwartsHouse}</h2>
-                <h4 class="character--childs">Children: <br>${character.children}</h4>
+                <h4 class="character--childs">Children: 
+                <br> ${character.children ? character.children.join(', ') : 'None'}</h4>
                 <p class="played-by">Played by: <br>${character.interpretedBy}</p>
-                <h3 class="character--birthdate">Born:   ${character.birthdate}</h3>                        
-                </div>
-            </div>`
-}
-
-function characterInfoHTML(character) {
-    return `<div class="character--img__wrapper">
-                <figure>
-                    <img class="character--img src="${character.image}" alt="">
-                </figure>
-            </div>
-            <div class="character--details">
-               
-            </div>`
-}
+                <h3 class="character--birthdate">Born: ${character.birthdate}</h3>                        
+              </div>
+            </div>`;
+  }
+  
+  async function handleSearch() {
+    const query = searchInputEl.value.toLowerCase();
+    const characters = await fetchCharacters();
+    const filteredCharacters = characters.filter(character => 
+      character.fullName.toLowerCase().includes(query) ||
+      character.hogwartsHouse.toLowerCase().includes(query) ||
+      character.interpretedBy.toLowerCase().includes(query)
+    );
+    displayCharacters(filteredCharacters);
+  }
+  
+  searchInputEl.addEventListener("input", handleSearch);
+  
+  main();
+  
